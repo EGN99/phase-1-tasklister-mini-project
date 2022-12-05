32c12d92ca993ab98a9f385f62eca8575200da08
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2022-12-05 13:16:45 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.getElementById('create-task-form');
+  // On submitting the form, carry out the creating of new todo item
+  form.addEventListener('submit', (event) => {
+      // Prevent the default action of the submit button reloading the page
+    event.preventDefault();
+    // Add todo to list
+    addToDo();
+    
+    // Clear the form after submitting
+    form.reset();
+  })
 });
+function addToDo(){
+
+  // Get the input text from the input field and save it in a variable
+  let todo = document.getElementById('new-task-description').value
+
+  // Create a paragraph element to hold the value
+  let itemList = document.createElement('li');
+  itemList.textContent = todo;
+  itemList.textContent = `${todo} `;
+}
